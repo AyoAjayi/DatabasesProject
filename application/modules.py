cb681b08d0329487a50eb824b4543f5db32254dc
Ayo Ajayi <ayomide.ajayi2839@gmail.com>
2021-12-20 02:21:48 -0500

    finished db setup

--- application/modules.py
+++ application/modules.py
@@ -1,1 +1,16 @@
-from application import db
+# from application import db
+
+import mysql.connector
+#replace the username and password to that of your db. Comment out this line and don't delete it for my db.
+mydb = mysql.connector.connect(host="localhost", user="root", password="cool1234", database="mydb")
+mycursor = mydb.cursor()
+# mycursor.execute("show databases")
+mycursor.execute("select * from country")
+
+#this will print out all databases
+for i in mycursor:
+    print(i)
+
+#when we start making procedures
+#cursor.callproc()
+# cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
