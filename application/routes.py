6f72d13eafe1b560d78f72b3dc3f69896b41465a
Ayo Ajayi <ayomide.ajayi2839@gmail.com>
2021-12-20 22:59:54 -0500

    formatted data

--- application/routes.py
+++ application/routes.py
@@ -7,21 +7,21 @@ import mysql.connector
 
 @app.route("/", methods=['GET', 'POST'])
 def myapp():
-    mydb = mysql.connector.connect(host="localhost", user="root", password="DA25wn!*", database="final_project")
+    mydb = mysql.connector.connect(host="localhost", user="root", password="cool1234", database="mydb")
     form = MyForm()
     if form.validate_on_submit():
         mycursor = mydb.cursor()
         country = form.country.data
         region = form.region.data
         print(form.query.data)
+        country_name = []
+        population = []
         if form.query.data == "1":
             args = (0, 0)
-            result = mycursor.callproc('current_country_pop', args)
+            result = mycursor.callproc('current_country_population', args)
             print (result[0])
             query = "SELECT country_name, population FROM Country INNER JOIN Population ON Population.country=Country.country_code WHERE year = '2019'"
             mycursor.execute(query)
-            for i in mycursor:
-                print(i)
         elif form.query.data == "2":
             mycursor.callproc("current_region_population")
         elif form.query.data == "3":
@@ -35,7 +35,10 @@ def myapp():
         else:
             mycursor.callproc("current_region_population")
         for i in mycursor:
-            print(i)
+            country_name.append(i[0])
+            population.append(i[1])
+        print(country_name)
+        print(population)
     return render_template('index.html',form=form)
 
 if __name__ == "__main__":
